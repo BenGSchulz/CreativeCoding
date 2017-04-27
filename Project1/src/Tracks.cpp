05ba3e0d1f09e6f8170e25b6feb6b27c7333c400
Benjamin Schulz <benjamin.schulz@du.edu>
2017-04-27 11:54:59 -0600

    Added Tracks class and implemetation

---
+++ Project1/src/Tracks.cpp
@@ -0,0 +1,9 @@
+//
+//  Tracks.cpp
+//  Project1
+//
+//  Created by Ben Schulz on 4/27/17.
+//
+//
+
+#include "Tracks.hpp"
---
+++ Project1/src/Tracks.hpp
@@ -0,0 +1,14 @@
+//
+//  Tracks.hpp
+//  Project1
+//
+//  Created by Ben Schulz on 4/27/17.
+//
+//
+
+#ifndef Tracks_hpp
+#define Tracks_hpp
+
+#include <stdio.h>
+
+#endif /* Tracks_hpp */
